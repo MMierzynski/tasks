6f6d02ad35035972d0144f18dab0595099b09438
Mateusz Mierzyński <mateusz.mateusz992@gmail.com>
2018-02-14 11:06:21 +0100

    poprawki wyglądu

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+
+        if($this->isGranted("ROLE_USER"))
+        {
+            return $this->redirectToRoute('my_task_index');
+        }
+
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
