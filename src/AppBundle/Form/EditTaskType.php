c5fa324f9e74e97030264b8c43c9edbaf4c8b4d7
Mateusz Mierzyński <mateusz.mateusz992@gmail.com>
2018-02-25 13:21:52 +0100

    p

---
+++ src/AppBundle/Form/EditTaskType.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mateusz
+ * Date: 25.02.18
+ * Time: 13:20
+ */
+
+namespace AppBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+
+class EditTaskType extends AbstractType
+{
+
+
+
+
+
+}
